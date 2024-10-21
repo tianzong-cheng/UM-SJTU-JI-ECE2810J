ef639dd80ac5ba58a5daf30eca93b02cb19bc660
Tianzong Cheng <oierprime@outlook.com>
2024-10-21 15:30:32 +0800

    feat: format p1 header

--- p1/sort.hpp
+++ p1/sort.hpp
@@ -1,37 +1,37 @@
 #ifndef VE281P1_SORT_HPP
 #define VE281P1_SORT_HPP
 
-#include <vector>
-#include <stdlib.h>
 #include <functional>
+#include <stdlib.h>
+#include <vector>
 
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void bubble_sort(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
 
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void insertion_sort(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
 
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void selection_sort(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
 
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void merge_sort(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
 
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void quick_sort_extra(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
-template<typename T, typename Compare>
+template <typename T, typename Compare>
 void quick_sort_inplace(std::vector<T> &vector, Compare comp = std::less<T>()) {
-    // TODO: implement
+  // TODO: implement
 }
 
-#endif //VE281P1_SORT_HPP
+#endif // VE281P1_SORT_HPP
